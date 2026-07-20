83a28646a1e688c53636da3cfc43c41c88b48619
Alejandro Silva <alejandro.felipe@outlook.com>
2026-07-19 21:32:34 -0300

    docs: clarify FillFluidWithNBTTransfer bridges potion contents onto the item

--- src/main/java/slimeknights/mantle/fluid/transfer/FillFluidWithNBTTransfer.java
+++ src/main/java/slimeknights/mantle/fluid/transfer/FillFluidWithNBTTransfer.java
@@ -13,7 +13,7 @@ import slimeknights.mantle.Mantle;
 import slimeknights.mantle.recipe.helper.ItemOutput;
 import slimeknights.mantle.recipe.ingredient.FluidIngredient;
 
-/** Fluid transfer info that fills a fluid into an item, copying its NBT */
+/** Fluid transfer info that fills a fluid into an item, bridging the fluid's potion contents onto the item */
 public class FillFluidWithNBTTransfer extends FillFluidContainerTransfer {
   public static final ResourceLocation ID = Mantle.getResource("fill_nbt");
   public FillFluidWithNBTTransfer(Ingredient input, ItemOutput filled, FluidIngredient fluid) {
